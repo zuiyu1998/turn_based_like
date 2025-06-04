73f4e4557f621456464aaeb9b46d2b0c858985f6
zuiyu1998 <1542844298@qq.com>
2025-06-04 22:26:06 +0800

    feat: 添加属性的更改

--- src/common/attribute_system/attribute.gd
+++ src/common/attribute_system/attribute.gd
@@ -10,10 +10,7 @@ extends Resource
 @export_multiline var description: String = ""
 ## 基础属性值
 @export
-var base_value: float = 0.0:
-    set(v):
-        base_value = v
-        _base_value = v
+var base_value: float = 0.0
 ## 属性允许的最小值
 @export var min_value: float = - INF
 ## 属性允许的最大值
@@ -22,16 +19,66 @@ var base_value: float = 0.0:
 @export var can_be_negative: bool = false
 
 
-var _base_value: float = 0.0
-
 # 当前属性值
 var _current_value: float = 0.0
 
 # 修改器列表
-var modifier_list: Array[AttributeModifier] = []
+var _modifier_list: Array[AttributeModifier] = []
+
+
+func set_base_value(v: float):
+    base_value = v
+    _recalculate_current_value()
+
+
+func add_modifier(modifier: AttributeModifier):
+    if not modifier in _modifier_list:
+        _modifier_list.append(modifier)
+        _recalculate_current_value()
+
+
+func remove_modifier(modifier: AttributeModifier):
+    if modifier in _modifier_list:
+        _modifier_list.erase(modifier)
+        _recalculate_current_value()
+
+
+func _recalculate_current_value():
+    var modifier_list := _modifier_list.duplicate()
+    
+    var absolute_modifier_list = []
+    var override_modifier
+    var percentage_modifier_list = []
+
+    for modifier: AttributeModifier in modifier_list:
+        match modifier.operation:
+            AttributeModifier.ModifierOperation.ABSOLUTE:
+                absolute_modifier_list.push_back(modifier)
+            AttributeModifier.ModifierOperation.OVERRIDE:
+                override_modifier = modifier
+            AttributeModifier.ModifierOperation.PERCENTAGE:
+                percentage_modifier_list.push_back(modifier)
+
+    for modifier: AttributeModifier in absolute_modifier_list:
+        modifier.apply(self)
+
+    for modifier: AttributeModifier in percentage_modifier_list:
+        modifier.apply(self)
+
+    if not override_modifier:
+        override_modifier.apply(self)
+
+    var final_value = _current_value
+
+    if not can_be_negative and final_value < 0.0:
+        final_value = 0.0
+    
+    final_value = clampf(final_value, min_value, max_value)
+    _current_value = final_value
+
 
 func get_base_value() -> float:
-    return _base_value
+    return base_value
 
 
 func get_current_value() -> float:
